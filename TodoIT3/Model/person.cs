2a74c46b69d7d0b2f566c066b99885aaf1223d08
Rand Farhan <rand.farhan@gmail.com>
2019-11-12 15:55:59 +0100

    name

---
+++ TodoIT3/Model/person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIT3.Model
+{
+    class person
+    {
+
+        string '=' (("firstname")+" "+("secondname")); 
+
+    }
+}
